53477adb14855918ce38e7269111593ef66e9e8b
Samrat798 <majumdersamrat26@gmail.com>
2024-10-25 23:59:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+let taskList = [];
+let edit_ID = null;
+
+const inputDOM = document.querySelector('.todo-input');
+const addBtn = document.querySelector('.add');
+const btnText = addBtn.innerText;
+const taskListDOM = document.querySelector('.list-container');
+
+window.addEventListener('load', getFromLocalStorage());
+addBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  addTask();
+});
+taskListDOM.addEventListener('click', function (e) {
+  if (e.target.classList.contains('delete')) {
+    deleteTask(e.target.parentNode.getAttribute('data-key'));
+  }
+  if (e.target.classList.contains('edit')) {
+    editTask(e.target.parentNode.getAttribute('data-key'));
+  }
+});
+
+function addTask() {
+  const task = inputDOM.value;
+  if (!task) return alert('Please enter an task.');
+  if (editTask != null) {
+    taskList.find((todo) => {
+      if (todo.id == edit_ID) {
+        todo.task = task;
+      }
+    });
+  }
+  if (edit_ID == null) {
+    const newTask = {
+      task,
+      id: Date.now(),
+      complete: false,
+    };
+    taskList.push(newTask);
+  }
+  inputDOM.value = '';
+  saveToLocalStorage(taskList);
+  edit_ID = null;
+  addBtn.innerText = btnText;
+}
+
+function displayTask(tasks) {
+  taskListDOM.innerHTML = '';
+  tasks.map((task) => {
+    const li = document.createElement('li');
+    li.classList.add('task');
+    li.setAttribute('data-key', task.id);
+    li.innerHTML = `
+      <input type="checkbox" class="checkbox" />
+            ${task.task}
+            <button class="btn delete">Delete</button>
+            <button class="btn edit">Edit</button>
+        `;
+    taskListDOM.appendChild(li);
+  });
+}
+
+function saveToLocalStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  displayTask(tasks);
+}
+
+function getFromLocalStorage() {
+  const tasks = localStorage.getItem('tasks');
+  if (tasks) {
+    taskList = JSON.parse(tasks);
+    displayTask(taskList);
+  }
+}
+
+function deleteTask(id) {
+  taskList = taskList.filter((task) => task.id != id);
+  saveToLocalStorage(taskList);
+}
+function editTask(id) {
+  edit_ID = id;
+  let task = taskList.find((task) => task.id == id);
+  inputDOM.focus();
+  inputDOM.value = task.task;
+  addBtn.textContent = 'Done';
+}
